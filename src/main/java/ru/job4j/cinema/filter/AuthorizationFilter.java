23ce11fa6790544a3296904afca30aab13aa2cef
Evgeny Novoselov <enovoselov@yahoo.com>
2023-07-10 16:40:20 +0300

    Поправил работу фильтра авторизации по умолчанию блокирует доступ

--- src/main/java/ru/job4j/cinema/filter/AuthorizationFilter.java
+++ src/main/java/ru/job4j/cinema/filter/AuthorizationFilter.java
@@ -14,15 +14,25 @@ import java.util.Set;
 @Component
 @Order(1)
 public class AuthorizationFilter extends HttpFilter {
-    private static final Set<String> URLS_PROTECT = Set.of(
-            "/tickets"
+    private static final Set<String> URLS_GUEST = Set.of(
+            "/users/register",
+            "/users/login",
+            "/film-sessions",
+            "/films",
+            "/webjars",
+            "/files"
     );
 
     @Override
     protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String uri = request.getRequestURI();
+        boolean isMainPage = uri.equals("/");
+        if (isMainPage || isAlwaysPermitted(uri)) {
+            chain.doFilter(request, response);
+            return;
+        }
         boolean userLoggedIn = request.getSession().getAttribute("user") != null;
-        if (isUrlProtect(uri) && !userLoggedIn) {
+        if (!userLoggedIn) {
             String loginPageUrl = request.getContextPath() + "/users/login";
             response.sendRedirect(loginPageUrl);
             return;
@@ -30,7 +40,7 @@ public class AuthorizationFilter extends HttpFilter {
         chain.doFilter(request, response);
     }
 
-    private boolean isUrlProtect(String uri) {
-        return URLS_PROTECT.stream().anyMatch(uri::startsWith);
+    private boolean isAlwaysPermitted(String uri) {
+        return URLS_GUEST.stream().anyMatch(uri::startsWith);
     }
 }
